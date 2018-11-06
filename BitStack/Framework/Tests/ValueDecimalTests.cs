adb065aeba1d440f85c8190742fbd9d04fca4005
David Arayan <david.arayan12@gmail.com>
2018-11-06 17:00:04 +1100

    - Fix compiler warning

--- BitStack/Framework/Tests/ValueDecimalTests.cs
+++ BitStack/Framework/Tests/ValueDecimalTests.cs
@@ -10,11 +10,11 @@ using BitStack;
 public class ValueDecimalTests {
 	static readonly float FLOAT_SMALL_POSITIVE = float.Epsilon;
 	static readonly float FLOAT_SMALL_NEGATIVE = -float.Epsilon;
-	static readonly float FLOAT_ZERO;
+	static readonly float FLOAT_ZERO = 0f;
 
 	static readonly double DOUBLE_SMALL_POSITIVE = double.Epsilon;
 	static readonly double DOUBLE_SMALL_NEGATIVE = -double.Epsilon;
-	static readonly double DOUBLE_ZERO;
+	static readonly double DOUBLE_ZERO = 0f;
 
 	[Test]
 	public void Test_FloatEqualTolerance() {
