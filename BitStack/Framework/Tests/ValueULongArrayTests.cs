3deb438c056df9bea3af439c2c51c9c924177074
David Arayan <david.arayan12@gmail.com>
2018-12-04 00:52:14 +1100

    - Added ValueULongArrayExtensions and equivalent Unit Tests. - Everything working as intended. - TO/DO will need to add unit tests for adding/reading byte data into arrays - This branch is now ready to merge into master

---
+++ BitStack/Framework/Tests/ValueULongArrayTests.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using BitStack;
+using System;
+
+public class ValueULongArrayTests {
+	const int BITS = 64;
+	
+	static readonly ulong[] TEST_VALUE_ARRAY = {12893967776213, 
+												19667342348, 
+												2139819384576987, 
+												254453537345767, 
+												96872, 
+												24398874, 
+												276679832,
+												254453537345767};
+												
+	static readonly int LOOP_COUNT = BITS * TEST_VALUE_ARRAY.Length;
+	static readonly int LOOP_COUNT_BYTES = LOOP_COUNT / BITS;
+	static readonly int[] EXPTECTED_BITS = CalcBits(TEST_VALUE_ARRAY);
+	
+	// NOTE -> This is Tested elsewhere and is assumed correct	
+	private static int[] CalcBits(ulong[] value) {
+		int[] bits = new int[BITS * value.Length];
+		
+		int index = 0;
+		
+		for (int i = 0; i < value.Length; i++) {
+			for (int j = 0; j < BITS; j++) {
+				bits[index] = value[i].BitAt(j);
+				index++;
+			}
+		}
+		
+		return bits;
+	}
+	
+	// NOTE -> This is Tested elsewhere and is assumed correct	
+	private static ulong[] GetTestArray() {
+		ulong[] copyArray = new ulong[TEST_VALUE_ARRAY.Length];      
+		Array.Copy(TEST_VALUE_ARRAY, copyArray, TEST_VALUE_ARRAY.Length);
+		return copyArray;
+	}
+	
+	[Test]
+	public void Test_BitAt() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			int bit = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(bit == EXPTECTED_BITS[i], 
+							"Expected Bit(" + bit + ") at Index(" + i + ") to be " + EXPTECTED_BITS[i]);
+		}
+	}
+	
+	[Test]
+	public void Test_BitInvAt() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			int bit = TEST_VALUE.BitInvAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitInvAt(i) != EXPTECTED_BITS[i], 
+							"Expected Bit(" + bit + ") at Index(" + i + ") not to be " + EXPTECTED_BITS[i]);
+		}
+	}
+
+	[Test]
+	public void Test_SetBitAt() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+		}
+	}
+
+	[Test]
+	public void Test_UnsetBitAt() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.UnsetBitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+		}
+	}
+	
+	[Test]
+	public void Test_SetBit() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBit(i, 0);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+						 
+			TEST_VALUE.SetBit(i, 1);
+						 
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+		}
+	}
+	
+	[Test]
+	public void Test_SetUnsetBit() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBit(i, 0);
+			TEST_VALUE.SetBit(i, 1);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+						 
+			TEST_VALUE.SetBit(i, 1);
+			TEST_VALUE.SetBit(i, 0);
+						 
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+		}
+	}
+
+	[Test]
+	public void Test_ToggleBitAt() {
+		ulong[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.ToggleBitAt(i);
+			
+			int inv = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) != EXPTECTED_BITS[i],
+							"Expected Toggle(" + inv + ") and InvTest(" + EXPTECTED_BITS[i] + ") to not Match.");
+		}
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.ToggleBitAt(i);
+			
+			int inv = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == EXPTECTED_BITS[i],
+							"Expected Toggle(" + inv + ") and InvTest(" + EXPTECTED_BITS[i] + ") to Match.");
+		}
+	}
+}
