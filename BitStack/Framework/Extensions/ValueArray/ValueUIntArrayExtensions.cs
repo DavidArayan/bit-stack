e409f3b25552e3adbd786fc6482b1e75639ef7e7
David Arayan <david.arayan12@gmail.com>
2018-12-04 00:37:52 +1100

    - Added ValueUIntArrayExtensions and equivalent Unit Tests - Everything working as intended

---
+++ BitStack/Framework/Extensions/ValueArray/ValueUIntArrayExtensions.cs
@@ -0,0 +1,242 @@
+﻿namespace BitStack {
+	
+	/**
+	 * An Extension of the ValueUIntExtension which allows working with bits
+	 * on an array of values. All functionality is piped directly to the
+	 * ValueUIntExtension whenever possible. 
+	 *
+	 * Since Arrays are a reference type, some functions will not return 
+	 * anything and will modify the array value directly.
+	 *
+	 * NOTICE ABOUT PERFORMANCE
+	 * 
+	 * UNITY_EDITOR or DEBUG flags ensure that common errors are caught. These
+	 * flags are removed in production mode so don't rely on try/catch methods.
+	 * If performing benchmarks, ensure that the flags are not taken into account.
+	 * The flags ensure that common problems are caught in code and taken care of.
+	 */
+	public static class ValueUIntArrayExtensions {
+		const int BIT_LEN = 32;
+		const int BYTE_LEN = BIT_LEN / 8;
+		
+		/**
+		 * Return the state of the bit (either 1 or 0) at provided
+		 * position. position value must be between [0, data.Length * 32]
+		 */
+		public static int BitAt(this uint[] data, int pos) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].BitAt(int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].BitAt(int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].BitAt(int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			return data[bitIndex].BitAt(pos % BIT_LEN);
+		}
+		
+		/**
+		 * Return the inverted state of the bit (either 1 or 0) at provided
+		 * position. position value must be between [0, data.Length * 32]
+		 */
+		public static int BitInvAt(this uint[] data, int pos) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].BitInvAt(int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].BitInvAt(int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].BitInvAt(int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			return data[bitIndex].BitInvAt(pos % BIT_LEN);
+		}
+
+		/**
+		 * Sets the state of the bit into the ON/1 at provided
+		 * position. position value must be between [0, data.Length * 32]
+		 */
+		public static void SetBitAt(this uint[] data, int pos) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].SetBitAt(int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].SetBitAt(int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].SetBitAt(int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			data[bitIndex] = data[bitIndex].SetBitAt(pos % BIT_LEN);
+		}
+
+		/**
+		 * Sets the state of the bit into the OFF/0 at provided
+		 * position. position value must be between [0, data.Length * 32]
+		 */
+		public static void UnsetBitAt(this uint[] data, int pos) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].UnsetBitAt(int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].UnsetBitAt(int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].UnsetBitAt(int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			data[bitIndex] = data[bitIndex].UnsetBitAt(pos % BIT_LEN);
+		}
+
+		/**
+		 * Toggles the state of the bit into the ON/1 or OFF/0 at provided
+		 * position. position value must be between [0, data.Length * 32].
+		 */
+		public static void ToggleBitAt(this uint[] data, int pos) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].ToggleBitAt(int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].ToggleBitAt(int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].ToggleBitAt(int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			data[bitIndex] = data[bitIndex].ToggleBitAt(pos % BIT_LEN);
+		}
+		
+		/**
+		 * Sets the state of the bit into the OFF/0 or ON/1 at provided
+		 * position. position value must be between [0, data.Length * 32]
+		 */
+		public static void SetBit(this uint[] data, int pos, int bit) {
+			int bitIndex = pos / BIT_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].SetBit(int, int) - bit position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].SetBit(int, int) - array is null");
+				}
+				
+				if (bitIndex > data.Length) {
+					BitDebug.Exception("uint[].SetBit(int, int) - bit bucket must not be greater than array " + data.Length + " was " + bitIndex);
+				}
+			#endif
+			
+			// pipe into the single value type
+			data[bitIndex] = data[bitIndex].SetBit(pos % BIT_LEN, bit);
+		}
+
+		/**
+		 * Count the number of set bits in the provided uint array (32 bits)
+		 * A general purpose Hamming Weight or popcount function which returns the number of
+		 * set bits in the argument.
+		 */
+		public static int PopCount(this uint[] data) {
+			#if UNITY_EDITOR || DEBUG
+				if (data == null) {
+					BitDebug.Exception("uint[].PopCount() - array is null");
+				}
+			#endif
+			
+			int counter = 0;
+			int length = data.Length;
+			
+			for (int i = 0; i < length; i++) {
+				counter += data[i].PopCount();
+			}
+			
+			return counter;
+		}
+
+		/**
+		 * Returns the byte (8 bits) at provided position index
+		 * Position value must be between [0, data.Length * 4]
+		 */
+		public static byte ByteAt(this uint[] data, int pos) {
+			int byteIndex = pos / BYTE_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].ByteAt(int) - byte position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].ByteAt(int) - array is null");
+				}
+				
+				if (byteIndex > data.Length) {
+					BitDebug.Exception("uint[].ByteAt(int) - byte bucket must not be greater than array " + data.Length + " was " + byteIndex);
+				}
+			#endif
+			
+			return data[byteIndex].ByteAt(pos % BYTE_LEN);
+		}
+
+		/**
+		 * Sets and returns the byte (8 bits) at provided position index
+		 * Position value must be between [0, data.Length * 4]
+		 */
+		public static void SetByteAt(this uint[] data, byte newData, int pos) {
+			int byteIndex = pos / BYTE_LEN;
+			
+			#if UNITY_EDITOR || DEBUG
+				if (pos < 0) {
+					BitDebug.Exception("uint[].SetByteAt(byte, int) - byte position must not be less than 0 was " + pos);
+				}
+				
+				if (data == null) {
+					BitDebug.Exception("uint[].SetByteAt(byte, int) - array is null");
+				}
+				
+				if (byteIndex > data.Length) {
+					BitDebug.Exception("uint[].SetByteAt(byte, int) - byte bucket must not be greater than array " + data.Length + " was " + byteIndex);
+				}
+			#endif
+			
+			data[byteIndex] = data[byteIndex].SetByteAt(newData, pos % BYTE_LEN);
+		}
+	}
+}
---
+++ BitStack/Framework/Tests/ValueUIntArrayTests.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using BitStack;
+using System;
+
+public class ValueUIntArrayTests {
+	const int BITS = 32;
+	
+	static readonly uint[] TEST_VALUE_ARRAY = {12893965, 19667344, 21398193, 25445352, 968721, 243988740, 276679832};
+	static readonly int LOOP_COUNT = BITS * TEST_VALUE_ARRAY.Length;
+	static readonly int LOOP_COUNT_BYTES = LOOP_COUNT / BITS;
+	static readonly int[] EXPTECTED_BITS = CalcBits(TEST_VALUE_ARRAY);
+	
+	// NOTE -> This is Tested elsewhere and is assumed correct	
+	private static int[] CalcBits(uint[] value) {
+		int[] bits = new int[BITS * value.Length];
+		
+		int index = 0;
+		
+		for (int i = 0; i < value.Length; i++) {
+			for (int j = 0; j < BITS; j++) {
+				bits[index] = value[i].BitAt(j);
+				index++;
+			}
+		}
+		
+		return bits;
+	}
+	
+	// NOTE -> This is Tested elsewhere and is assumed correct	
+	private static uint[] GetTestArray() {
+		uint[] copyArray = new uint[TEST_VALUE_ARRAY.Length];      
+		Array.Copy(TEST_VALUE_ARRAY, copyArray, TEST_VALUE_ARRAY.Length);
+		return copyArray;
+	}
+	
+	[Test]
+	public void Test_BitAt() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			int bit = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(bit == EXPTECTED_BITS[i], 
+							"Expected Bit(" + bit + ") at Index(" + i + ") to be " + EXPTECTED_BITS[i]);
+		}
+	}
+	
+	[Test]
+	public void Test_BitInvAt() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			int bit = TEST_VALUE.BitInvAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitInvAt(i) != EXPTECTED_BITS[i], 
+							"Expected Bit(" + bit + ") at Index(" + i + ") not to be " + EXPTECTED_BITS[i]);
+		}
+	}
+
+	[Test]
+	public void Test_SetBitAt() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+		}
+	}
+
+	[Test]
+	public void Test_UnsetBitAt() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.UnsetBitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+		}
+	}
+	
+	[Test]
+	public void Test_SetBit() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBit(i, 0);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+						 
+			TEST_VALUE.SetBit(i, 1);
+						 
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+		}
+	}
+	
+	[Test]
+	public void Test_SetUnsetBit() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.SetBit(i, 0);
+			TEST_VALUE.SetBit(i, 1);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == 1,
+						 "Expected Bit Position(" + i + ") to be 1");
+						 
+			TEST_VALUE.SetBit(i, 1);
+			TEST_VALUE.SetBit(i, 0);
+						 
+			Debug.Assert(TEST_VALUE.BitAt(i) == 0,
+						 "Expected Bit Position(" + i + ") to be 0");
+		}
+	}
+
+	[Test]
+	public void Test_ToggleBitAt() {
+		uint[] TEST_VALUE = GetTestArray();
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.ToggleBitAt(i);
+			
+			int inv = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) != EXPTECTED_BITS[i],
+							"Expected Toggle(" + inv + ") and InvTest(" + EXPTECTED_BITS[i] + ") to not Match.");
+		}
+		
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			TEST_VALUE.ToggleBitAt(i);
+			
+			int inv = TEST_VALUE.BitAt(i);
+			
+			Debug.Assert(TEST_VALUE.BitAt(i) == EXPTECTED_BITS[i],
+							"Expected Toggle(" + inv + ") and InvTest(" + EXPTECTED_BITS[i] + ") to Match.");
+		}
+	}
+}
