8a5548f3ae50d1e927d75045b636cba55150da61
David Arayan <david.arayan12@gmail.com>
2020-07-19 11:10:14 +1000

    - Consistency change

--- BitStack/Framework/Tests/ValueDecimalTests.cs
+++ BitStack/Framework/Tests/ValueDecimalTests.cs
@@ -10,11 +10,11 @@ using UnityEngine;
 public static class ValueDecimalTests {
     static readonly float FLOAT_SMALL_POSITIVE = float.Epsilon;
     static readonly float FLOAT_SMALL_NEGATIVE = -float.Epsilon;
-    static readonly float FLOAT_ZERO;
+    static readonly float FLOAT_ZERO = 0.0f;
 
     static readonly double DOUBLE_SMALL_POSITIVE = double.Epsilon;
     static readonly double DOUBLE_SMALL_NEGATIVE = -double.Epsilon;
-    static readonly double DOUBLE_ZERO;
+    static readonly double DOUBLE_ZERO = 0.0;
 
     [Test]
     public static void Test_FloatEqualTolerance() {
